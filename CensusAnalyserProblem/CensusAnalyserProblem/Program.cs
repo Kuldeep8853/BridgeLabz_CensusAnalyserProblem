60ab3547e0e891e0827c718fe6c88a0473328c12
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-03-03 12:53:25 +0530

    [Kuldeep]Add. Start and Welcome message

---
+++ CensusAnalyserProblem/CensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome the Census Analyser Program");
+        }
+    }
+}
---
+++ CensusAnalyserProblem/UnitTesting/UnitTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
